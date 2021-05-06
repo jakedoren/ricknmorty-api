ea1c338e993240de0f432390f26e0d78aacfd456
Jacob Doren <Jacob Doren>
2021-05-05 22:53:16 -0400

    housekeeping

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,3 @@
-// module.exports = {
-//     mongoURI: 'mongodb+srv://jakedoren:k2kRW#r2QksQLaY@cluster0.ogefg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// }
 
 module.exports = {
     mongoURI: 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PW + '@cluster0.ogefg.mongodb.net/solaropposites?retryWrites=true&w=majority'
